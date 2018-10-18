acc23ee8a91f3d8f799c0e466a0118c3a4b8b9af
giovanninp <gnp@cesar.school>
2018-10-17 22:49:55 -0300

    Questao 1 respondida

--- questoes/questao_1.py
+++ questoes/questao_1.py
@@ -27,9 +27,71 @@
 # Para a correta execução do programa, a estrutura atual deve ser mantida,
 # substituindo apenas o comando print(questão...) existente.
 ##
+
+'''Prof. Icaro, me desculpe pelo uso de funçoes, sei que sai do escopo dos assuntos atuais, porem acabei utilizando,
+por conta de organizacao e ficar mais pratica a correcao de erros.'''
+
+'''A seguinte funcao esta responsavel pela separacao das senhas, ela ira remover os espacos e ira verificar, a partir 
+das virgulas, o inicio e o fim de uma senha, alem de levar em consideracao o fim da string.'''
+def separador(text):
+    text = text.replace(" ", "")
+    end = 0
+    start = 0
+
+    while start < len(text):
+        senhaTemp = ""
+        while end < len(text)+1:
+            if (text[end] is "," or end == len(text)-1):
+                senhaTemp = senhaTemp.replace(",", "")
+                senhaTemp = text[start:end]
+                passChecker(senhaTemp)
+                start = end
+                end += 1
+            end += 1
+        start += 1
+
+"""O passChecker e responsavel pela verificacao, ela e chamada pelo separador, pois, como nao e utilizada uma lista,
+as verificacoes ocorrem a partir da criacao de cada "senha temporaria" a partir do string texto. """
+def passChecker(password):
+    password = password.replace(",","")
+    note = ""
+    result = False
+    if(len(password)<6):
+        note = ("Password muito curto!")
+    elif(len(password)>12):
+        note = ("Password muito longo!")
+    else:
+        if(password.isdigit()):
+            note = ("Password sem letras")
+        elif(password.isalpha()):
+            note = ("Password sem numeros")
+        else:
+            if(password.isupper()):
+                note = ("Password sem minusculos")
+            elif(password.islower()):
+                note = ("Password sem maiusculos")
+            else:
+                if"#" in password or "@" in password or "$" in password :
+                    print("Password perfeito:\n")
+                    result = True
+
+                else:
+                    note = ("Password sem caracteres especiais")
+    if(result == True):
+        print(password)
+
+    '''else:
+        print (password)
+        print(note)'''
+
+
+x = input()
+print(separador(x))
+
+
 def main():
-    print("questao 1")
-    
+    x = input("Digite um texto ou uma senha:\n")
+    print(separador(x))
 
 
 if __name__ == '__main__':
